072380283738e62b8aa5d08f2a601968a7fd47f9
OrsiniBr <udochukwugodwin13@gmail.com>
2025-12-17 23:50:33 +0100

    add experimental

--- dapp/next.config.ts
+++ dapp/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["pino", "thread-stream"],
+  },
 };
 
 export default nextConfig;
